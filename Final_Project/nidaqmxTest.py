68b3b73b5d8bc15f558451d22b0312fdf0ad27ad
bsumner137 <molonlabe137@gmail.com>
2018-04-27 23:52:38 -0600

    Closeout April 27

--- Final_Project/nidaqmxTest.py
+++ Final_Project/nidaqmxTest.py
@@ -1,20 +1,6 @@
-import nidaqmx
+import numpy as np
 import matplotlib.pyplot as plt
-
-plt.ion()
-
-i=0;
-
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai0:1")
-    while True:
-        data = task.read(number_of_samples_per_channel=1)
-        plt.scatter(i, data[0], c='r')
-        plt.scatter(i, data[1], c='b')
-        plt.pause(0.05)
-        i += 1
-        print(data)
-
+import nidaqmx
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
@@ -25,28 +11,27 @@ fs = 1000.0
 freqs = np.linspace(0, fs/2, nfft/2+1)
 
 with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config = TerminalConfiguration.RSE, min_val = -5.0, max_val = 5.0, units=VoltageUnits.VOLTS)
-    task.timing.cfg_samp_clk_timing(fs, active_edge=Edge.RISING, sample_mode=AquisitionType.CONTINUOUS, samps_per_chan=nfft)
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.RSE, min_val=-5.0, max_val=5.0, units=VoltageUnits.VOLTS)
+    task.timing.cfg_samp_clk_timing(fs, active_edge=Edge.RISING, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=nfft)
 
-    #task.start()
-    #task.stop()
+    # task.start()
+    # task.stop()
     while True:
 
-        t1 = (int(round(time.time()*1000)))
+        t1 = (int(round(time.time() * 1000)))
         data = task.read(number_of_samples_per_channel=nfft)
-        t2 = (int(round(time.time()*1000)))
+        t2 = (int(round(time.time() * 1000)))
         tdata = np.array(data)
         tdata -= np.mean(tdata)
-##        ffto = np.fft.fft(tdata)
-##        ffto = np.abs(ffto)
-##        ffto = ffto[0:(nfft/2+1)]
-        ffto = np.fft.fftshift(np.fft.fft(tdata))
+        ffto = np.fft.fft(tdata)
+        ffto = np.abs(ffto)
+        ffto = ffto[0:(nfft/2+1)]
 
         ax1.clear()
         ax2.clear()
         ax1.plot(data)
-        ax2.plot(freqs, fft0)
+        ax2.plot(freqs, ffto)
         fig.show(0)
         plt.pause(0.000000001)
-        t3 = (int(round(time.time()*1000)))
-        print('t2-t1 = {}ms, t3-t2 = {}ms'.format(t2-t1, t3-t2))
+        t3 = (int(round(time.time() * 1000)))
+        print 't2-t1 = {}ms, t3-t2 ={}ms'.format(t2-t1, t3-t2)
